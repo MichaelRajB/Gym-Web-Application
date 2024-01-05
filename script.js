64f788daee9084e2870487c4540c76062966ba00
MichaelRajB <155736938+MichaelRajB@users.noreply.github.com>
2024-01-05 19:22:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Add your logic to handle form submission (e.g., send data to server, display a confirmation message)
+    alert("Form submitted successfully!");
+  });
+  
